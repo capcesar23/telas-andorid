dc888a1829820808adb0a82b252c809b15f94ba5
capcesar23 <cesar_a_pereira@hotmail.com>
2022-11-24 04:18:05 -0300

    Telas android

---
+++ app/src/main/java/com/example/telas/LaranjaPretoActivity.java
@@ -0,0 +1,14 @@
+package com.example.telas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaranjaPretoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laranja_preto);
+    }
+}
